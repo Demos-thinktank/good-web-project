0d7b523de47cb3d90737d388ab35488f1a3de28d
Maria Olanipekun <m.olanipekun@hotmail.com>
2020-09-15 14:25:17 +0100

    deployment fix

--- src/pages/join-in/index.js
+++ src/pages/join-in/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import IframeResizer from "iframe-resizer-react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
